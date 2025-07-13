eeb8d4db8436cb6a700689ce428a7bb261f1e1a8
Amayas HADDAG <amayas.haddag@etu.u-paris.fr>
2025-07-13 03:39:45 +0200

    Modified marketplace structure

--- blockchain/contracts/Universe.sol
+++ blockchain/contracts/Universe.sol
@@ -7,14 +7,26 @@ import "./NFT.sol";
 contract Universe {
     enum Item {
         FanToken,
-        NFT
+        NFT,
+        Physical
     }
 
     struct MarketplaceItem {
         Item itemType;
         address seller;
+        address tokenAddress;
         uint256 price;
         uint256 amountOrTokenId;
+
+        string title;
+        string description;
+
+        bool rewardNFT;
+
+        bool rewardFanToken;
+
+        bool rewardCrypto;
+        uint256 rewardAmount;
     }
 
     address private owner;
@@ -45,12 +57,21 @@ contract Universe {
         Item itemType,
         address tokenAddress,
         uint256 amountOrTokenId,
-        uint256 price
+        uint256 price,
+
+        string memory title,
+        string memory description,
+
+        bool rewardNFT,
+        bool rewardFanToken,
+        bool rewardCrypto,
+
+        uint256 rewardAmount
     ) external {
         require(price > 0, "Price must be greater than 0");
 
         if (itemType == Item.NFT) {
-            ERC721URIStorage nftContract = ERC721URIStorage(tokenAddress);
+            NFT nftContract = NFT(tokenAddress);
 
             require(nftContract.ownerOf(amountOrTokenId) == msg.sender, "You do not own this NFT");
 
@@ -60,38 +81,77 @@ contract Universe {
                 "Marketplace not approved for NFT"
             );
 
-            marketplaceItems[marketplaceItemCount] = MarketplaceItem({
-                itemType: Item.NFT,
-                seller: msg.sender,
-                price: price,
-                amountOrTokenId: amountOrTokenId
-            });
-
             nftContract.transferFrom(msg.sender, address(this), amountOrTokenId);
         }
 
         else if (itemType == Item.FanToken) {
-            IERC20 tokenContract = IERC20(tokenAddress);
+            FanToken tokenContract = FanToken(tokenAddress);
 
             require(tokenContract.balanceOf(msg.sender) >= amountOrTokenId, "Insufficient fan token balance");
             require(tokenContract.allowance(msg.sender, address(this)) >= amountOrTokenId, "Marketplace not approved for tokens");
 
-            marketplaceItems[marketplaceItemCount] = MarketplaceItem({
-                itemType: Item.FanToken,
-                seller: msg.sender,
-                price: price,
-                amountOrTokenId: amountOrTokenId
-            });
-
             tokenContract.transferFrom(msg.sender, address(this), amountOrTokenId);
         }
 
-        else {
-            revert("Unsupported item type");
-        }
+        marketplaceItems[marketplaceItemCount] = MarketplaceItem({
+            itemType: Item.Physical,
+            seller: msg.sender,
+            tokenAddress: tokenAddress,
+            price: price,
+            amountOrTokenId: amountOrTokenId,
+            title: title,
+            description: description,
+            rewardNFT: rewardNFT,
+            rewardFanToken: rewardFanToken,
+            rewardCrypto: rewardCrypto,
+            rewardAmount: rewardAmount
+        });
 
         marketplaceItemCount++;
     }
 
+    function buyItem(uint256 itemId) external payable {
+        MarketplaceItem memory item = marketplaceItems[itemId];
+        require(item.price > 0, "Item does not exist");
+        require(msg.sender != item.seller, "Cannot buy your own item");
+        require(msg.value == item.price, "Incorrect ETH sent");
+
+        payable(item.seller).transfer(msg.value);
+
+        if (item.itemType == Item.NFT) {
+            NFT nftContract = NFT(item.tokenAddress);
+            nftContract.safeTransferFrom(address(this), msg.sender, item.amountOrTokenId);
+        }
+
+        else if (item.itemType == Item.FanToken) {
+            FanToken tokenContract = FanToken(item.tokenAddress);
+            tokenContract.transfer(msg.sender, item.amountOrTokenId);
+        }
+
+        else if (item.itemType == Item.Physical) {
+            if (item.rewardNFT) {
+                rewardNFTContract.mintNFT(msg.sender, "ipfs://example-metadata-uri");
+            }
+
+            if (item.rewardFanToken) {
+                psgToken.mint(msg.sender, item.rewardAmount); // ou autre club selon logique métier
+            }
+
+            if (item.rewardCrypto) {
+                require(address(this).balance >= item.rewardAmount, "Not enough ETH for reward");
+                payable(msg.sender).transfer(item.rewardAmount);
+            }
+        }
+
+        delete marketplaceItems[itemId];
+    }
+
+    function getMarketplaceItems() external view returns (MarketplaceItem[] memory) {
+        MarketplaceItem[] memory items = new MarketplaceItem[](marketplaceItemCount);
+        for (uint256 i = 0; i < marketplaceItemCount; i++) {
+            items[i] = marketplaceItems[i];
+        }
+        return items;
+    }
 
 }
